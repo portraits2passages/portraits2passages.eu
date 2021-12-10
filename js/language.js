a921adee74195670d527ccaf63bcc897859fd373
Nicolas Burger <burgerni10@outlook.fr>
2021-12-10 20:40:52 +0100

    Revert "feat(design): basic design"
    This reverts commit f7f18432b9326190b513fb609c4e274c8751a171.

--- js/language.js
+++ js/language.js
@@ -1,35 +1,38 @@
-function setCookie(name, value, daysExpiration = 1) {
-  const d = new Date()
-  d.setTime(d.getTime() + daysExpiration * 86400000)
-  const expires = 'expires=' + d
+function setCookie(name, value, exdays) {
+  var d, expires
+  exdays = exdays || 1
+  d = new Date()
+  d.setTime(d.getTime() + exdays * 86400000)
+  expires = 'expires=' + d
   document.cookie = name + '=' + value + '; ' + expires + '; path=/'
 }
 
 function getCookie(name) {
-  const cookies = document.cookie.split('; ')
-  for (let i = 0; i < cookies.length; i++) {
-    let c = cookies[i].split('=')
-    if (c[0] === name) {
+  var c
+  cookies = document.cookie.split(';')
+  for (var i = 0; i < cookies.length; i++) {
+    c = cookies[i].split('=')
+    if (c[0] == name) {
       return c[1]
     }
   }
   return ''
 }
 
-const standardLanguage = {}
-let standardLanguageLoaded = false
+var standardLanguage = {}
+var standardLanguageLoaded = false
 
 function loadJSON(file, callback) {
-  const xmlRequest = new XMLHttpRequest()
-  xmlRequest.overrideMimeType('application/json')
-  xmlRequest.open('GET', file, true) // Replace 'my_data' with the path to your file
-  xmlRequest.onreadystatechange = function() {
-    if (xmlRequest.readyState === 4 && xmlRequest.status === 200) {
+  var xobj = new XMLHttpRequest()
+  xobj.overrideMimeType('application/json')
+  xobj.open('GET', file, true) // Replace 'my_data' with the path to your file
+  xobj.onreadystatechange = function() {
+    if (xobj.readyState == 4 && xobj.status == '200') {
       // Required use of an anonymous callback as .open will NOT return a value but simply returns undefined in asynchronous mode
-      callback(JSON.parse(xmlRequest.responseText))
+      callback(JSON.parse(xobj.responseText))
     }
   }
-  xmlRequest.send(null)
+  xobj.send(null)
 }
 
 function chooseLanguage(lang, loadStandard = false) {
@@ -68,6 +71,8 @@ if (!document.cookie) {
   if (lang !== '') {
     if (lang === 'fr-FR') {
       chooseLanguage('fr-FR', true)
+    } else {
+      console.log('lang', lang)
     }
   }
 }
